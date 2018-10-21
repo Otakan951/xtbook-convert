85f4cf42caab81d95bf890d32ef66d3799b4c54c
Otakan951 <otakan951@gmail.com>
2018-10-21 22:56:53 +0900

    変換中のログの出力先変更

--- article.sh
+++ article.sh
@@ -10,6 +10,7 @@ for script in `ls ${basePath}/xtbconv/scripts/wikis`; do
   outBundleName=${name}-${DATE}.xtbdict
   outBundle=${derivedPath}/xtbdict/${outBundleName}
   compressionPath=${derivedPath}/7z/${full_name}/${DATE}
+  wikiplexusLogPath=${derivedPath}/logs/wikiplexus-${DATE}.log
 
   echo "Name = \"${name}:${full_name}\"" >> ${logPath}
 
@@ -26,21 +27,22 @@ for script in `ls ${basePath}/xtbconv/scripts/wikis`; do
   echo "plistPath = \"${plistPath}\"" >> ${logPath}
   echo "outBundle = \"${outBundle}\"" >> ${logPath}
   echo "compressionPath = \"${compressionPath}\"" >> ${logPath}
+  echo "wikiplexusLogPath = \"${wikiplexusLogPath}\"" >>${logPath}
 
   mkdir ${outBundle}
 
   curl --retry 5 -s ${xmlUrl} | \
   7z x -si${xml} -so | \
-  ${MkXTBWikiplexusPath} -o ${outBundle} -s ${wikiplexusOptions} 2>> ${logPath} | \
-  ${MkRaxPath} -o ${outBundle}/Articles.db.rax 2>> ${logPath} >> ${logPath}
-  ${YomiGenesisPath} < ${outBundle}/BaseNames.csv > ${outBundle}/Yomis.csv 2>> ${logPath}
-  ${MkXTBIndexDBPath} -o ${outBundle}/Search ${outBundle}/Yomis.csv 2>> ${logPath}
+  ${MkXTBWikiplexusPath} -o ${outBundle} -s ${wikiplexusOptions} 2>> ${wikiplexusLogPath} | \
+  ${MkRaxPath} -o ${outBundle}/Articles.db.rax 2>> ${logPath} >> ${wikiplexusLogPath}
+  ${YomiGenesisPath} < ${outBundle}/BaseNames.csv > ${outBundle}/Yomis.csv 2>> ${wikiplexusLogPath}
+  ${MkXTBIndexDBPath} -o ${outBundle}/Search ${outBundle}/Yomis.csv 2>> ${wikiplexusLogPath}
 
   rm -f ${outBundle}/BaseNames.csv ${outBundle}/Titles.csv
   cp ${plistPath} ${outBundle}/Info.plist 2>> ${logPath}
 
   size=`du -m ${outBundle} |awk '{print $1}'`
-  echo "変換前のファイルサイズ:${size}MB" >> ${logPath}
+  echo "圧縮前のファイルサイズ:${size}MB" >> ${logPath}
   #source ${basePath}/xtbdconv/scripts/compression.sh
 done
 
