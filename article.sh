b3f637063986ef06165150c277d24e06742ad4bf
Otakan951 <otakan951@gmail.com>
2018-10-22 17:55:05 +0900

    細かい修正

--- article.sh
+++ article.sh
@@ -9,16 +9,16 @@ for script in `ls ${basePath}/xtbconv/scripts/wikis`; do
   plistPath=${basePath}/xtbconv/plists/${name}/Info.plist
   outBundleName=${name}-${DATE}.xtbdict
   outBundle=${derivedPath}/xtbdict/${outBundleName}
-  compressionPath=${derivedPath}/7z/${full_name}/${DATE}
+  compressionPath=${derivedPath}/compression/${full_name}/${DATE}
   wikiplexusLogPath=${derivedPath}/logs/wikiplexus-${DATE}.log
 
   echo "Name = \"${name}:${full_name}\"" >> ${logPath}
 
   if [ -e ${outBundle} -o -e ${compressionPath}/${outBundleName}.7z* ]; then
-    echo "${name}は既に変換済みです。{name}の変換をスキップします。" >> ${logPath}
+    echo "${name}は既に変換済みです。${name}の変換をスキップします。" >> ${logPath}
     continue
   elif [ ! -e ${plistPath} ]; then
-    echo "Info.plistが見つかりません。{name}の変換をスキップします。" >> ${logPath}
+    echo "Info.plistが見つかりません。${name}の変換をスキップします。" >> ${logPath}
     continue
   fi
 
@@ -34,16 +34,16 @@ for script in `ls ${basePath}/xtbconv/scripts/wikis`; do
   curl --retry 5 -s ${xmlUrl} | \
   7z x -si${xml} -so | \
   ${MkXTBWikiplexusPath} -o ${outBundle} -s ${wikiplexusOptions} 2>> ${wikiplexusLogPath} | \
-  ${MkRaxPath} -o ${outBundle}/Articles.db.rax 2>> ${logPath} >> ${wikiplexusLogPath}
+  ${MkRaxPath} -o ${outBundle}/Articles.db.rax 2>> ${wikiplexusLogPath} >> ${wikiplexusLogPath}
   ${YomiGenesisPath} < ${outBundle}/BaseNames.csv > ${outBundle}/Yomis.csv 2>> ${wikiplexusLogPath}
   ${MkXTBIndexDBPath} -o ${outBundle}/Search ${outBundle}/Yomis.csv 2>> ${wikiplexusLogPath}
 
   rm -f ${outBundle}/BaseNames.csv ${outBundle}/Titles.csv
-  cp ${plistPath} ${outBundle}/Info.plist 2>> ${logPath}
+  cp ${plistPath} ${outBundle}/Info.plist
 
   size=`du -m ${outBundle} |awk '{print $1}'`
   echo "圧縮前のファイルサイズ:${size}MB" >> ${logPath}
-  #source ${basePath}/xtbdconv/scripts/compression.sh
+  #source ${basePath}/xtbconv/scripts/compression.sh
 done
 
 #source ${basePath}/xtbconv/scripts/upload.sh
